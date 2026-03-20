dbd85501ed753e1b38a328276318b7531a97e82c
kumkum sisodiya <kumkumsisodiya6232@gmail.com>
2026-03-20 14:49:27 +0530

    first commit

---
+++ routes/listing.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router =  express.Router();
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const {isLoggedIn, isOwner,validateListing} = require("../middleware.js");
+const multer =require('multer');
+const {storage}= require("../cloudConfig.js");
+const upload = multer({storage})
+const listingController =require("../controllers/listing.js");
+
+
+router
+   .route("/")
+   .get(wrapAsync(listingController.index) // index route
+    ) 
+   .post( //create route
+       isLoggedIn, 
+       upload.single('listing[image]'),
+       validateListing ,
+       wrapAsync(listingController.createListing)
+    )
+  
+
+//new route
+router.get("/new", isLoggedIn, listingController.renderNewForm);    
+
+router
+    .route("/:id")
+    .get(   // show route
+       wrapAsync(listingController.showListings)
+     )
+    .put( // update
+       isLoggedIn, 
+       isOwner,
+        upload.single('listing[image]'),
+       validateListing ,
+       wrapAsync(listingController.updateListing)
+     )
+    .delete( // delete route
+       isLoggedIn, 
+       isOwner,
+       wrapAsync(listingController.destroyListing)
+     )
+
+
+//edit route
+router.get(
+     "/:id/edit",
+     isLoggedIn, 
+     isOwner, 
+     wrapAsync(listingController.editListing)
+);
+
+module.exports = router;
